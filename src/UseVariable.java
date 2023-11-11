69f1c7d417291b9244bea0bb6d970626909729f6
tomknk <tomita.kanako.feb@gmail.com>
2023-11-12 07:42:50 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,13 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number1;
+        String str1;
+        number1 = 100;
+        str1 = "xyz";
+        System.out.print(number1);
+        System.out.print(str1);
+    }
+
+}
